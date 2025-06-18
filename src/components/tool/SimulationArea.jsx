cc908eb50e15591c465ef4ac1d15cbb67fe7cb6d
Atm <117608077+atmbr@users.noreply.github.com>
2025-06-17 23:20:09 -0300

    Update SimulationArea.jsx

--- src/components/tool/SimulationArea.jsx
+++ src/components/tool/SimulationArea.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import CommandSimulator from '../CommandSimulator';
+import { CommandSimulator }from '@/components/CommandSimulator';
 
 const SimulationArea = ({ result }) => (
   <AnimatePresence>
